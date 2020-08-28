92546c516c47011e7900a45562e34f8a39a3ede6
alex-muliande <amuliande@gmail.com>
2020-08-28 22:09:11 +0300

    Add content in javascript the hide and show effect.

--- js/script.js
+++ js/script.js
@@ -8,5 +8,24 @@
      $("#design-pic").slideDown();
      $(".design-a").hide();
    })
+   $("#prod-pic").click(function(){
+     $(".prod-a").hide(1500).toggle();
+     $("#prod-pic").slideUp ();
+       });
+       $(".prod-a").click(function(){
+         $("#prod-pic").slideDown();
+         $(".prod-a").hide();
+       })
+
+   $("#develop-pic").click(function(){
+     $(".develop-a").hide(1500).toggle();
+     $("#develop-pic").slideUp();
+   });
+  $(".develop-a").click(function(){
+    $("#develop-pic").slideDown();
+    $(".develop-a").hide();
+  })
+
+
 
  })
